3d51994c219186c943956c07814413c67790f711
Minhao Zhang <minhao.zhang@qq.com>
2017-01-05 16:51:47 +0800

    * init ut

---
+++ ut/DynamoDbTableTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minhao
+ * Date: 2017-01-05
+ * Time: 16:29
+ */
+
+namespace Oasis\Mlib\AwsWrappers\Test;
+
+use Oasis\Mlib\AwsWrappers\DynamoDbTable;
+
+class DynamoDbTableTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  DynamoDbTable */
+    protected $table;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+    
+}
---
+++ ut/ut-bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minhao
+ * Date: 2017-01-05
+ * Time: 16:23
+ */
+use Oasis\Mlib\AwsWrappers\Test\UTConfig;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+UTConfig::load();
